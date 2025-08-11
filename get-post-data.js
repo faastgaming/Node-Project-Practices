677dee862918aa3c1997f91377365aed5d628f17
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-11 13:16:11 +0330

    Create get-post-data.js

---
+++ get-post-data.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const qs = require('querystring');
+
+const server = http.createServer((req,res) => {
+    if(req.url == '/' && req.method == 'GET') {
+        res.setHeader('Content-Type' , 'text/html')
+        res.write(`
+            <html>
+                <head>
+                    <title>Roocket Website</title>
+                </head>
+                <body>
+                    <h1>Welcome to Roocket website</h1>
+                    <form action="" method="POST">
+                        <label for="serach">Search</label>
+                        <input name="search" id="search"/>
+                        <button>send Data</button>
+                    </form>
+                </body>
+            </html>
+        `)
+        return res.end();
+    } else if(req.url == '/' && req.method == 'POST') {
+        let body = '';
+        req.on('data' , (data) => {
+            body += data;
+        })
+
+        req.on('end' , () => {
+            let data = qs.parse(body);
+            console.log('my data' , data.search);
+            res.writeHead(302 , {'Location' : '/'})
+            res.end();
+        })
+
+    } else if(req.url == '/my-product') {
+        let mydata = { name : 'hesam' , family : 'mousavi'}
+        res.setHeader('Content-Type' , 'application/json');
+        res.write(JSON.stringify(mydata))
+        res.writeHead()
+        return res.end();
+    }
+})
+
+server.listen(3000 , '127.0.0.1' , () => {
+    console.log('server running on localhost:3000...')
+})
+
