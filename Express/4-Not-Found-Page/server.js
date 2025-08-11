3a9def35b793d62b807523807773dfc207c48757
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-11 15:27:24 +0330

    Update server.js

--- Express/4-Not-Found-Page/server.js
+++ Express/4-Not-Found-Page/server.js
@@ -1,1 +1,25 @@
-a
+const express = require('express');
+const app = express();
+const path = require('path')
+
+let myloggersystem = (req, res , next) => {
+    console.log('LOG');
+    next();
+}
+
+// app.use(myloggersystem)
+
+app.get('/' , myloggersystem , (req, res) => {
+    res.sendFile(path.join(__dirname , 'views/index.html'))
+})
+
+app.get('/products' , (req, res) => {
+    res.sendFile(path.join(__dirname , 'views/products.html'))
+})
+
+app.use((req,res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'))
+})
+
+
+app.listen(3000 , () => console.log('server is running ...'))
